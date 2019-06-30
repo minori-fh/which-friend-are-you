97d6540472604cd69fa7ddfe6be2232d1a2d3985
Minori Hashimoto <minori.fh@gmail.com>
2019-06-29 19:06:44 -0700

    create html routes in html.js

---
+++ server.js
@@ -0,0 +1,24 @@
+// Dependencies
+// =============================================================
+var express = require("express");
+
+// Sets up the Express App
+// =============================================================
+var app = express();
+var PORT = process.env.PORT || 3000;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+// ROUTER pointing our server to a series of route files
+// require("./app/routing/apiRoutes")(app);
+require("./app/routing/htmlRoutes")(app);
+
+// =============================================================================
+// LISTENER to "start" our server
+// =============================================================================
+
+app.listen(PORT, function() {
+    console.log("App listening on PORT: " + PORT);
+  });
