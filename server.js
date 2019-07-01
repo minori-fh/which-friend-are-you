97f9c0c7726d583923d4795ca5527ce90b917161
Minori Hashimoto <minori.fh@gmail.com>
2019-07-01 09:50:12 -0700

    make app folder static to link style.css

--- server.js
+++ server.js
@@ -8,6 +8,9 @@ var app = express();
 var PORT = process.env.PORT || 3000;
 
 // Sets up the Express app to handle data parsing
+// app.use(express.static('app'));
+// app.use(express.static(__dirname + 'app'));
+app.use(express.static('app'));
 app.use(express.urlencoded({ extended: true }));
 app.use(express.json());
 
